30d4b73dc7dddc464a8ff9dfcf73374c74e7475d
Kakeru Tsukayama <halxvi@gmail.com>
2020-02-07 18:16:50 +0900

    first commit

---
+++ database/migrations/2020_02_07_063214_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->bigincrements('id');
+            $table->unsignedBigInteger('userid');
+            $table->foreign('userid')->references('userid')->on('users');
+            $table->unsignedBigInteger('facilityid');
+            $table->foreign('facilityid')->references('facilityid')->on('facilities');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
