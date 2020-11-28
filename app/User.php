e37b6a2193589a1765ad7e3456127ed542678aef
halxvi <halxvi@gmail.com>
2020-11-28 19:27:31 +0900

    delete unnecessary comments

--- app/User.php
+++ app/User.php
@@ -14,13 +14,13 @@ class User extends Model
         // Create&Save時に自動でidをインサート
         self::creating(function ($model) {
             if (empty($model->userid)) {
-                $model->userid = Uuid::generate()->string; // UUIDを作成、挿入
+                $model->userid = Uuid::generate()->string;
             }
         });
 
         self::saving(function ($model) {
             if (empty($model->userid)) {
-                $model->userid = Uuid::generate()->string; // UUIDを作成、挿入
+                $model->userid = Uuid::generate()->string;
             }
         });
     }
