dc2b54b275db652741b28f49fc514a7639d07835
halxvi <halxvi@gmail.com>
2020-12-06 23:06:37 +0900

    add comments

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,15 +1,18 @@
 @extends('layout')
 
 @section('header')
-      <link rel="stylesheet" href="{{asset('css/main.css')}}">
-      <script>
-        let api_token = '<?php echo $api_token[0]; ?>';
-    </script>
+  <link rel="stylesheet" href="{{asset('css/main.css')}}">
+  <script>
+    let api_token = '<?php echo $api_token[0]; ?>';
+  </script>
 @endsection
 
 @section('content')
-<div id="main"></div>
-<input type='hidden' value={{ $api_token[0] }}>
-<script src="{{mix('js/app.js')}}"></script>
-<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+  <div id="main"></div>
+
+  //応急処置
+  <input type='hidden' value={{ $api_token[0] }}>
+
+  <script src="{{mix('js/app.js')}}"></script>
+  <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
 @stop
